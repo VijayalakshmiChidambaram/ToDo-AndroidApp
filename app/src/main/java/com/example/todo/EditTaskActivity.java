cc71a44f3ccdd4c7fcf219c4b01a42016bddac79
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-02-06 23:33:55 -0600

    Replaced a better gif

--- app/src/main/java/com/example/todo/EditTaskActivity.java
+++ app/src/main/java/com/example/todo/EditTaskActivity.java
@@ -1,8 +1,5 @@
 package com.example.todo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -10,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
